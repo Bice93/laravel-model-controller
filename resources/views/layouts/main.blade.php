3eaebedf98b04b65281e2e763494628db990aea1
Bice93 <bianca.cerulo@hotmail.it>
2022-09-08 17:27:23 +0200

    Style card movie

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies</title>
-
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    
     @yield('head-scripts')
 </head>
 <body>
